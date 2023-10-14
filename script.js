2cb0af092c326840e6f61ce4943376e48e6f8409
Anwin Antony <91240875+Anwin-127@users.noreply.github.com>
2023-10-14 22:48:40 +0530

    Add files via upload
    Responsive Web

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 window.addEventListener('scroll',function(){
     const header = document.querySelector('header');
-    header.classList.toggle("sticky", window.scrollY > 10);
+    const themeToggle = document.getElementById('themeToggle');
+    header.classList.toggle("sticky", window.scrollY > 0);
+    themeToggle.classList.toggle("sticky", window.scrollY > 0);
 });
 
-const themeToggle = document.getElementById('themeToggle');
 let isDarkMode = true;
 themeToggle.addEventListener('click', () => {
     if (isDarkMode) {
@@ -12,11 +13,25 @@ themeToggle.addEventListener('click', () => {
         root.style.setProperty('--text-color','black')
         themeToggle.src = 'moon.png';
         isDarkMode = false;
+        root.style.setProperty('--list-color','black')
+        root.style.setProperty('--switch-on','0')
+        root.style.setProperty('--switch-off','1')
     } else {
         var root = document.querySelector(':root');
         root.style.setProperty('--bg-color','black')
         root.style.setProperty('--text-color','white')
+        root.style.setProperty('--list-color','white')
         themeToggle.src = 'sun.png';
         isDarkMode = true;
+        root.style.setProperty('--switch-on','1')
+        root.style.setProperty('--switch-off','0')
     }
 });
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
